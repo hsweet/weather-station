e4bc265ad38bc3632e9b3fb024627469ec5441d9
Harry Sweet <hsweet52@gmail.com>
2017-05-20 10:29:08 -0400

    Update rain.py
    adds logging, uses interrupts on GPIO, new function to write daily rainfall total to a log file.  Nothing will happen until it rains.  There could be an issue if it does not rain for a month.  It will think it is still the same day.

--- rain.py
+++ rain.py
@@ -1,18 +1,52 @@
 import  RPi.GPIO as GPIO
 import time
-
+import datetime
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
-x=0
+# globals, dom is set to a date that is not in any month
+x, rainfall,dom = 0,0,0
+
+def is_new_day(day):
+    global dom
+    if dom != day:      # new day
+        dom = day
+        return True
+    else:
+        return False    # same day
 
 while True:
-    inputValue = GPIO.input(12)
+    #           Check for rain
+    
+    #inputValue = GPIO.input(12)                        #poll  or
+    inputValue = GPIO.wait_for_edge(12, GPIO.RISING)   #interupt
+    #inputValue = 0                                      #for testing
     if (inputValue == False):
-        #add if you need to count
-        #print(x)
         x=x+1
         rainfall=x*0.011
         print (rainfall)
-    time.sleep(0.05)
+    time.sleep(1)
+
+    #           Check for new day, save daily rainfall total
+    today = datetime.datetime.now()
+    next_day = is_new_day(today.day)
+    if next_day == True:
+        
+        #       add daily total to daily rain log
+        print ('Total rainfall for {}-{}-{} is {}'.format (today.month, today.day,today.year, rainfall))
+        try:
+            log = open('dailyrain.log' , 'a')
+            log.write('{} {:.2f}\n'.format(datetime.datetime.now().strftime("%Y-%m-%d"), rainfall))
+            log.close()
+        except:
+            print ('Cannot write to daily log file')
+    
+    #       write to log file
+    try:
+        log = open('rain.log', 'a')
+        log.write("{} {:.2f}  \n".format (datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S"), rainfall))
+        log.close()
+    except KeyboardInterrupt:
+         log.close()
+         exit('Thats all, folks')
