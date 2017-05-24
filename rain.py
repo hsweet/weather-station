8872d00a7dbd524e5f134eed3dab7395aa9b39f4
Harry Sweet <hsweet52@gmail.com>
2017-05-24 08:39:43 -0400

    Update rain.py
    Mostly cosmetic changes to make the code easier to understand.

--- rain.py
+++ rain.py
@@ -2,86 +2,80 @@
 import threading
 import time
 import datetime
-import random
 
 #GPIO.setmode(GPIO.BCM)
 #GPIO.setup(12, GPIO.IN,pull_up_down=GPIO.PUD_UP)
  
 rainfall = 0 
 
-def is_bigger(than):
-    pass
-    
+def daily_rainfall():
+    '''            Total daily rainfall log
 
-def is_new_day():
-    '''Keep a daily log of total rainfall
-
-    Wait for midnight, log the total rainfall for the day.
-    Set the dom variable to the new day, wait another 24 hours
+    Each midnight, log the total rainfall for the day.
+    Set the current_day variable to the new day, wait another 24 hours
     Runs in a separate thread so it will work even if the mail loop is
     not active, which is true when there is no rain
     '''
     now = datetime.datetime.now()
-    day = str(now.year) + str(now.month) + str(now.day)   #will change after midnight
-    dom, mostrain = 0, 0
+    new_day = str(now.year) + str(now.month) + str(now.day)   #will change after midnight
+    current_day, most_rain = 0, 0
     while True:
         now = datetime.datetime.now()
-        #day = random.randint(1,2)
-        day = now.minute
-        #day = str(now.year) + str(now.month) + str(now.day)   #will change after midnight
+        #new_day = now.minute                    # for testing .. changes each minute
+        new_day = str(now.year) + str(now.month) + str(now.day)   #will change after midnight
         
-        if rainfall > mostrain:
-            mostrain = rainfall
-        print ("Rainfall is {},total accumulation {}".format(rainfall, mostrain))
+        if rainfall > most_rain:
+            most_rain = rainfall
+        print ("Rainfall is {},total accumulation {}".format(rainfall, most_rain))
         
-        if dom != day:              # it's a new day
-            dom = day
-            print (day)
+        if current_day != new_day:              # it's a new day
+            current_day = new_day
             
-            print ('Total rainfall for {}-{}-{} is {}'.format (now.month, now.day,now.year, mostrain))
+            print ('Total rainfall for {}-{}-{} is {}'.format (now.month, now.day,now.year, most_rain))
             try:
                 log = open('dailyrain.log' , 'a')
-                log.write('{} {:.2f}\n'.format(datetime.datetime.now().strftime("%Y-%m-%d"), mostrain))
+                log.write('{} {:.2f}\n'.format(datetime.datetime.now().strftime("%Y-%m-%d"), most_rain))
                 log.close()
             except:
                 print ('Cannot write to daily log file')
-            mostrain = 0            # reset daily
-            time.sleep(10)
+            most_rain = 0            # reset daily
+            # time.sleep(10)
         else:
-            time.sleep(2)
-            print ('Waking up, still today, back to sleep'.format (rainfall))
-            #print (rainfall)
+            time.sleep(60)
+            print ('Waking up, it is still today, going back to sleep')
 
 
-def measure_rainfall():
-    x=0
+def read_rain_guage():
+    '''Read rain guage, write aculmulating log during a rain
+       Runs with an interupt so only runs when there is rain,  
+       Resets to zero when it detects a change of day
+
+    '''
+    x = 0
     now = datetime.datetime.now()
     day = str(now.year) + str(now.month) + str(now.day)   #will change after midnight
-    dom = 0 
+    current_day = 0 
     
     while True:
-        '''Accumulate total daily rainfall.  Write to daily log file
-           Needs code to reset rainfall to zero at midnight
-    '''    
         #inputValue = GPIO.input(12)                        #poll  or
         #inputValue = GPIO.wait_for_edge(12, GPIO.RISING)   #interupt
         inputValue = 0              # for testing only
 
-        now = datetime.datetime.now()
-        day = now.minute
         global rainfall
         ####################### Measure Rainfall ######################     
         if (inputValue == False):
             x = x + 1
-            rainfall = x*0.011
-            print (rainfall)
-        time.sleep(1)               # remove after testing
+            rainfall = x * 0.011
+            print (round(rainfall,4))
+        time.sleep(600)               # remove after testing
 
         ####################  Reset at Midnight #####################
-        if dom != day:
-            dom = day
+        now = datetime.datetime.now()
+        new_day = str(now.year) + str(now.month) + str(now.day)   #will change after midnight
+        # day = now.minute           # for faster testing
+        if current_day != new_day:
+            current_day = new_day
             x = 0
-            #print (day)
         ##################### write to log file ######################
         try:
             log = open('rain.log', 'a')
@@ -90,6 +84,7 @@ def measure_rainfall():
         except KeyboardInterrupt:
              log.close()
              exit('Thats all, folks')
+             
 
-threading.Thread(target = is_new_day).start()
-threading.Thread(target = measure_rainfall).start()
+threading.Thread(target = daily_rainfall).start()
+threading.Thread(target = read_rain_guage).start()
