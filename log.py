f228a73e5a8232f233c772b6597e0cf0ab5e0ed1
Harry Sweet <hsweet52@gmail.com>
2017-08-17 08:46:14 -0400

    Update log.py

--- log.py
+++ log.py
@@ -4,21 +4,21 @@ class Log:
     def __init__(self, logname):
         self.logname = logname
         self.description = 'Use: instance = Log(name)'
-        
+        self.path = '/var/www/html/'
+        self.fullname = self.path + self.logname
 
     def write(self, data):
         self.data = data
         try:
-            log = open (self.logname, 'a')
+            log = open (self.fullname, 'a')
             log.write(self.data + '\n')
         except:
             print('Cannot write to log file')
         log.close
         
-
+#******************************* sample instance ******************
 logname = 'temperature.log'
-temperature = 50
-frequency = 30
+temperature = time.time()
 t=Log(logname)
 print (t.description)
 
@@ -26,6 +26,4 @@ print (t.description)
 while True:
     
     t.write(str(temperature))
-    time.sleep(1)
-
-
+    time.sleep(5)
